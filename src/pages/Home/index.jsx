2fd07a24b24103f99a3cc60cc50508970f287450
Vitor Natario <natariovitor@gmail.com>
2022-08-17 17:25:44 -0300

    feat: Applying useState on component card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,22 @@
+import React, { useState } from "react";
 import "./styles.css";
 import { Card } from "../../components/Card";
 
 export function Home() {
+  const [studentName, setStudentName] = useState("");
+
   return (
     <div className="container">
-      <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome..." />
+      <h1>Nome: {studentName}</h1>
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={(e) => setStudentName(e.target.value)}
+      />
       <button type="burron">Adicionar</button>
 
       <Card name="Carlos" time="16:51:23" />
-      <Card name="Roberto" time="16:57:42" />
+      <Card name="Juliana" time="14:57:42" />
     </div>
   );
 }
