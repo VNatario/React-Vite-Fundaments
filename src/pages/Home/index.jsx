800937e76d6c1b10099d4e7f1394bc6e9f9a73dd
Vitor Natario <natariovitor@gmail.com>
2022-08-17 17:00:12 -0300

    feat: Create Card component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import "./styles.css";
+import { Card } from "../../components/Card";
 
 export function Home() {
   return (
@@ -6,6 +7,9 @@ export function Home() {
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome..." />
       <button type="burron">Adicionar</button>
+
+      <Card name="Carlos" time="16:51:23" />
+      <Card name="Roberto" time="16:57:42" />
     </div>
   );
 }
