02fce4db690059ba7935897fee2e10ec291b716a
Vitor Natario <natariovitor@gmail.com>
2022-08-18 17:06:02 -0300

    feat: typing component card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+type CardProps = {
+    name: string;
+    time: string;
+};
+
+export function Card({ name, time }: CardProps) {
+    return (
+        <div className="card">
+            <strong>{name}</strong>
+            <small>{time}</small>
+        </div>
+    );
+}
