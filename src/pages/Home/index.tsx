6981a696c0e01eeb67073d3c5ed0cdf35c38c4ac
Vitor Natario <natariovitor@gmail.com>
2022-08-18 18:12:47 -0300

    feat: typing request

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,69 +2,73 @@ import React, { useState, useEffect } from "react";
 import "./styles.css";
 import { Card, CardProps } from "../../components/Card";
 
+type ProfileResponse = {
+  name: string;
+  avatar_url: string;
+};
+
+type User = {
+  name: string;
+  avatar: string;
+};
 export function Home() {
-    const [studentName, setStudentName] = useState("");
-    const [students, setStudents] = useState<CardProps[]>([]);
-    const [user, setUser] = useState({ name: "", avatar: "" });
+  const [studentName, setStudentName] = useState("");
+  const [students, setStudents] = useState<CardProps[]>([]);
+  const [user, setUser] = useState<User>({} as User);
 
-    function handleAddStudent() {
-        const newStudent = {
-            name: studentName,
-            time: new Date().toLocaleTimeString("pt-BR", {
-                hour: "2-digit",
-                minute: "2-digit",
-                second: "2-digit",
-            }),
-        };
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-BR", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      }),
+    };
 
-        setStudents((prevState) => [...prevState, newStudent]);
-        setStudentName("");
+    setStudents((prevState) => [...prevState, newStudent]);
+    setStudentName("");
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("https://api.github.com/users/VNatario");
+        const data = (await response.json()) as ProfileResponse;
+
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url,
+        });
+      } catch (error) {
+        console.error(error);
+      }
     }
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await fetch(
-                    "https://api.github.com/users/VNatario"
-                );
-                const data = await response.json();
-                setUser({
-                    name: data.name,
-                    avatar: data.avatar_url,
-                });
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        fetchData();
-    }, []);
-
-    return (
-        <div className="container">
-            <header>
-                <h1>Lista de Presença</h1>
-                <div>
-                    <strong>{user.name}</strong>
-                    <img src={user.avatar} alt="Foto de perfil" />
-                </div>
-            </header>
-            <input
-                type="text"
-                placeholder="Digite o nome..."
-                value={studentName}
-                onChange={(e) => setStudentName(e.target.value)}
-            />
-            <button type="button" onClick={handleAddStudent}>
-                Adicionar
-            </button>
-
-            {students.map((student) => (
-                <Card
-                    key={student.time}
-                    name={student.name}
-                    time={student.time}
-                />
-            ))}
+  return (
+    <div className="container">
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de perfil" />
         </div>
-    );
+      </header>
+      <input
+        type="text"
+        placeholder="Digite o nome..."
+        value={studentName}
+        onChange={(e) => setStudentName(e.target.value)}
+      />
+      <button type="button" onClick={handleAddStudent}>
+        Adicionar
+      </button>
+
+      {students.map((student) => (
+        <Card key={student.time} name={student.name} time={student.time} />
+      ))}
+    </div>
+  );
 }
